1e7cd2dcfd89188a6f298c33bef5ddfb5fea052f
Tobarrientos2 <tobarrientos2@gmail.com>
2024-01-20 12:46:32 -0300

    second commit

--- src/stores/exStore.ts
+++ src/stores/exStore.ts
@@ -1,5 +1,5 @@
 import { readable } from "svelte/store";
-import {ex, cl} from "../data/ex.json"
+import {ex} from "../data/ex.json"
 
 //Obtener Servicios desde el JSON
 const data = { ex } || {};
@@ -12,14 +12,4 @@ export const exUnsubscribe = exReadable.subscribe(() => {
 } );
 
 
-//Obtener Servicios desde el JSON
-const clients = {cl} || {};
-//Crear Readable
-export const clReadable = readable({}, (set) => {
-    set(clients);
-});
 
-
-export const clUnsubscribe = exReadable.subscribe(() => {
-
-} );
